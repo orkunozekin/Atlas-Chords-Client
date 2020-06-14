81bb76d924b189144f417454c8a1a8760604eb74
Orkun Ozekin <orkunozekin@thinkful.com>
2020-06-13 23:26:10 -0500

    added main comp

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import SignIn from '../src/Sign-in/Sign-in'
 import Favorites from '../src/Favorites/Favorites'
 import SignUp from './Sign-up/Sign-up';
+import Main from './Main/Main';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route exact path='/sign-in' component={SignIn} />
       <Route exact path='/favorites' component={Favorites} />
       <Route exact path='/sign-up' component={SignUp} />
+      <Route exact path='/' component={Main}/>
     </section>
   );
 }
