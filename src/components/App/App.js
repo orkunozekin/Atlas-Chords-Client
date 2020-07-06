5d9ab0072f4434f32b8917e81b19dc39eff00030
Orkun Ozekin <orkunozekin@thinkful.com>
2020-07-05 20:07:30 -0500

    re-organized components' locations

---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Navbar from '../Navbar/Navbar'
+import SignUp from '../Sign-up/Sign-up'
+import submitNewChord from '../Submit-New-Chord/SubmitNewChord'
+import LoginForm from '../LoginForm/LoginForm'
+import LandingPage from '../Landing-Page/LandingPage'
+import Chord from '../Chord/Chord'
+import PrivateRoute from '../Utils/PrivateRoute'
+
+
+export default class App extends React.Component {
+
+
+  render() {
+    return (
+      <main className="App">
+        <Navbar />
+        <Route exact path='/log-in' component={LoginForm} />
+        <Route exact path='/sign-up' component={SignUp} />
+        <Route exact path='/' component={LandingPage} />
+        <PrivateRoute exact path='/submitNewChord' component={submitNewChord} />
+        <Route path='/chords/:id' component={Chord} />
+      </main>
+
+    );
+  }
+}
+
